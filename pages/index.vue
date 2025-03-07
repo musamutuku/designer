ade3b25f3b785b1b287e0385e53f550ceeb170d1
musajose <78262428+musamutuku@users.noreply.github.com>
2025-03-07 06:17:11 +0000

    not found

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
 <div class="flex items-center justify-center h-screen border border-black">
-  <p>Page content temporary unavailable. Contact Jose [+254799144429]</p>
+  <p class="px-2">Page content temporary unavailable. Contact Jose [+254799144429]</p>
 </div>
 </template>
