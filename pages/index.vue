cfeb05cc3c063959610e7e7a84c373fb7c3d481b
musajose <78262428+musamutuku@users.noreply.github.com>
2025-03-10 09:08:21 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
 <div class="flex items-center justify-center h-screen border border-black">
-  <p class="px-2">Page content temporary unavailable. Contact Jose [+254799144429]</p>
+  <p class="px-2">Page content temporary unavailable. Contact Jose</p>
 </div>
 </template>
